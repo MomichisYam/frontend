bd3e8333399e838bccbc376ae4c1abd08f593908
goattwest <gushlopezg2005@gmail.com>
2025-12-10 23:50:06 -0600

    Funcion de cerrar sesión añadida

--- js/tablero.js
+++ js/tablero.js
@@ -1,28 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const usuario = localStorage.getItem("UsuarioActivo");
-  const nombreDeUsuarioText = document.getElementById("usernameDisplay");
-  const letrasPerfil = document.getElementById("userAvatar");
+    const usuario = localStorage.getItem("UsuarioActivo");
+    const nombreDeUsuarioText = document.getElementById("usernameDisplay");
+    const letrasPerfil = document.getElementById("userAvatar");
 
-  //Le asignamos al boton de "Agregar Tarea" la funcion para que aparezca la pantalla donde ingresa los datos.
-  const botonAgregar = document.getElementById("botonAgregarTarea");
-  botonAgregar.addEventListener("click", formularioCreacionDeTareas);
+    //Le asignamos al boton de "Agregar Tarea" la funcion para que aparezca la pantalla donde ingresa los datos.
+    const botonAgregar = document.getElementById("botonAgregarTarea");
+    botonAgregar.addEventListener("click", formularioCreacionDeTareas);
 
-  //Le asignamos al div donde esta el nombre del usuario una funcion para abrir una lista desplegable
-  const perfilParte = document.getElementById("perfilParte");
-  perfilParte.addEventListener("click", abrirMenuDesplegablePerfil);
+    //Le asignamos al div donde esta el nombre del usuario una funcion para abrir una lista desplegable
+    const perfilParte = document.getElementById("perfilParte");
+    perfilParte.addEventListener("click", abrirMenuDesplegablePerfil);
 
-  cargarUsuario(usuario, nombreDeUsuarioText, letrasPerfil);
+    cargarUsuario(usuario, nombreDeUsuarioText, letrasPerfil);
 });
 
 function abrirMenuDesplegablePerfil() {
-  //Si no existe el menu, crea uno nuevo
-  if (!document.getElementById("menuDesplegablePerfil")) {
-    const menuDesplegable = document.createElement("div");
-    menuDesplegable.id = "menuDesplegablePerfil";
-    menuDesplegable.className = "menu-desplegable-perfil";
+    //Se hace otro div para que no haya problemas al presionar el boton que se crea, ya que este boton esta dentro del div de perfilParte
+    const partePerfil = document.getElementById("parteDePerfil");
 
-    //Contenido del html
-    menuDesplegable.innerHTML = `
+    //Si no existe el menu, crea uno nuevo
+    if (!document.getElementById("menuDesplegablePerfil")) {
+        const menuDesplegable = document.createElement("div");
+        menuDesplegable.id = "menuDesplegablePerfil";
+        menuDesplegable.className = "menu-desplegable-perfil";
+
+        //Contenido del html
+        menuDesplegable.innerHTML = `
             <ul>
                 <li id="cerrarSesion">
                     <button onclick= cerrarSesion()>Cerrar sesión</button>
@@ -30,72 +33,101 @@ function abrirMenuDesplegablePerfil() {
             </ul>
         `;
 
-    const perfilParte = document.getElementById("perfilParte");
-    perfilParte.appendChild(menuDesplegable);
+        partePerfil.appendChild(menuDesplegable);
 
-    //Si ya esta abierto el menu, se cerrará al darle click
-  } else {
-    const menuExistente = document.getElementById("menuDesplegablePerfil");
-    perfilParte.removeChild(menuExistente);
-  }
+        //Si ya esta abierto el menu, se cerrará al darle click
+    } else {
+        const menuExistente = document.getElementById("menuDesplegablePerfil");
+        partePerfil.removeChild(menuExistente);
+    }
 }
 
 function cargarUsuario(usuario, nombreDeUsuarioText, letrasPerfil) {
-  if (usuario) {
-    //Si encuentra un usuario en el LocalStorage cambia el nombre de la parte del header
-    nombreDeUsuarioText.textContent = usuario;
-    //Saca las dos primeras letras del nombre y las pone dentro de la "Imagen de perfil"
-    letrasPerfil.textContent = usuario.substring(0, 2).toUpperCase();
-  } else {
-    //Si no hay un usuario guardado en el localStorage se regresa al Login
-    alert("No se identificó un inicio de sesión, regresando al login");
-    window.location.href = "login.html";
-  }
+    if (usuario) {
+        //Si encuentra un usuario en el LocalStorage cambia el nombre de la parte del header
+        nombreDeUsuarioText.textContent = usuario;
+
+        //Saca las dos primeras letras del nombre y las pone dentro de la "Imagen de perfil"
+        letrasPerfil.textContent = usuario.substring(0, 2).toUpperCase();
+
+    } else {
+        //Si no hay un usuario guardado en el localStorage se regresa al Login
+        alert("No se identificó un inicio de sesión, regresando al login");
+        window.location.href = "login.html";
+    }
 }
 
-function cargarTareas() {}
+function cargarTareas() {
+
+}
 
 async function crearTarea(nombreTarea, tiempo) {
-  //Valida que los campos no esten vacios
-  if (nombreTarea.value.trim() === "" || tiempo.value.trim() === "") {
-    alert("Llena todos los campos");
-    return;
-  } else {
-    const nuevaTarea = {
-      taskName: nombreTarea.value,
-      durationMinutes: tiempo.value,
-    };
+    //Valida que los campos no esten vacios
+    if (nombreTarea.value.trim() === "" || tiempo.value.trim() === "") {
+        alert("Llena todos los campos");
+        return;
+    } else {
+        const nuevaTarea = {
+            taskName: nombreTarea.value,
+            durationMinutes: tiempo.value,
+        };
 
-    try {
-      const response = await fetch(`${CONFIG.API_URL}/pomodoros`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        body: JSON.stringify(nuevaTarea),
-      });
+        try {
+            const response = await fetch(`${CONFIG.API_URL}/pomodoros`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                credentials: "include",
+                body: JSON.stringify(nuevaTarea),
+            });
 
-      if (response.ok) {
-        const data = await response.json();
-        console.log("Sesión creada con ID:", data.id);
-      } else {
-        console.error("Error al crear sesión:", response.status);
-      }
-    } catch (error) {
-      console.error("Error de red:", error);
+            if (response.ok) {
+                const data = await response.json();
+                console.log("Sesión creada con ID:", data.id);
+            } else {
+                console.error("Error al crear sesión:", response.status);
+            }
+        } catch (error) {
+            console.error("Error de red:", error);
+        }
     }
-  }
+}
+
+async function cerrarSesion() {
+    try {
+        const response = await fetch(`${CONFIG.API_URL}/auth/logout`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include'
+        });
+
+        if (response.ok) {
+            alert("Cerrando sesión...");
+            
+            //Borramos el nombre que guardamos en el LocalStorage
+            localStorage.removeItem("UsuarioActivo");
+            
+            //Redirigir al usuario al login
+            window.location.href = "login.html";
+        } else {
+            console.error("Error al cerrar sesión:", response.status);
+        }
+    } catch (error) {
+        console.error("Error de red:", error);
+    }
 }
 
 //Función que crea la pantallita para el formulario de creación de tareas
 function formularioCreacionDeTareas() {
-  const ModalAgregar = document.createElement("div");
-  ModalAgregar.className = "pantalla-agregar";
-  ModalAgregar.id = "pantallaAgregar";
+    const ModalAgregar = document.createElement("div");
+    ModalAgregar.className = "pantalla-agregar";
+    ModalAgregar.id = "pantallaAgregar";
 
-  //Este es el html de la pantalla que aparece
-  ModalAgregar.innerHTML = `
+    //Este es el html de la pantalla que aparece
+    ModalAgregar.innerHTML = `
         <div class="contenido-pantalla-agregar">
             <h2>Agregar nueva tarea</h2>
             
@@ -112,15 +144,15 @@ function formularioCreacionDeTareas() {
         </div>
     `;
 
-  //Agregamos lo que creamos al body
-  document.body.appendChild(ModalAgregar);
-  document.getElementById("pantallaAgregar").focus();
+    //Agregamos lo que creamos al body
+    document.body.appendChild(ModalAgregar);
+    document.getElementById("pantallaAgregar").focus();
 }
 
 //Borra la pantalla del formulario que se crea arriba
 function cerrarFormularioCreacionDeTareas() {
-  const modal = document.getElementById("pantallaAgregar");
-  if (modal) {
-    document.body.removeChild(modal);
-  }
+    const modal = document.getElementById("pantallaAgregar");
+    if (modal) {
+        document.body.removeChild(modal);
+    }
 }
